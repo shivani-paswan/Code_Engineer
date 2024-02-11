0f5a97eed05f46a0d895814e4d62859c5b1d6d76
shivani <shivani123paswan@gmail.com>
2024-02-12 01:28:31 +0530

    starting project from starting by commenting firebase login

--- lib/auth/LoginScreen.dart
+++ lib/auth/LoginScreen.dart
@@ -1,5 +1,3 @@
-
-
 import 'dart:async';
 
 import 'package:code_engineer/BottomNavigation/Home.dart';
@@ -19,178 +17,217 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  bool loading=false;
+  bool loading = false;
 
-  final _formkey=GlobalKey<FormState>();
-  final userController=TextEditingController();
-  final passwordController=TextEditingController();
- final _auth=FirebaseAuth.instance;
+  final _formkey = GlobalKey<FormState>();
+  final userController = TextEditingController();
+  final passwordController = TextEditingController();
+  final _auth = FirebaseAuth.instance;
   @override
   void dispose() {
     // TODO: implement initState
     userController.dispose();
     passwordController.dispose();
     super.dispose();
-
-
   }
 
-  void login(){
-    setState((){
-      loading=true;
+  void login() {
+    setState(() {
+      loading = true;
     });
-  _auth.signInWithEmailAndPassword(email: userController.text.toString(), password: passwordController.text.toString()).
-  then((value) {
-    Utils().toastMessage(value.user!.email.toString()+ " Login successful");
-    setState((){
-      loading=false;
-      Timer(Duration(seconds: 1),  ()=> Navigator.push(context,MaterialPageRoute(builder: (context)=>HomePage())));
-      userController.clear();
-      passwordController.clear();
+    _auth
+        .signInWithEmailAndPassword(
+            email: userController.text.toString(),
+            password: passwordController.text.toString())
+        .then((value) {
+      Utils().toastMessage(value.user!.email.toString() + " Login successful");
+      setState(() {
+        loading = false;
+        Timer(
+            Duration(seconds: 1),
+            () => Navigator.push(
+                context, MaterialPageRoute(builder: (context) => HomePage())));
+        userController.clear();
+        passwordController.clear();
+      });
+    }).onError((error, stackTrace) {
+      Utils().toastMessage(error.toString());
+      setState(() {
+        loading = false;
+      });
     });
+  }
 
-  }).onError((error, stackTrace) {
-    Utils().toastMessage(error.toString());
-    setState((){
-      loading=false;
-    });
-  });
-}
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: () async{
-        SystemNavigator.pop();  // for exit from application
+      onWillPop: () async {
+        SystemNavigator.pop(); // for exit from application
         return true;
       },
       child: Scaffold(
-        backgroundColor:Colors.black ,
+        backgroundColor: Colors.black,
         appBar: AppBar(
           backgroundColor: Colors.black,
-         automaticallyImplyLeading: false,
+          automaticallyImplyLeading: false,
         ),
-        body:Container(
+        body: Container(
           padding: EdgeInsets.symmetric(horizontal: 20),
           child: SingleChildScrollView(
             child: Column(
               children: [
                 Container(
                   alignment: Alignment.center,
-
-                  child: CircleAvatar(
+                  child: const CircleAvatar(
                     radius: 50,
-                    backgroundImage:AssetImage('assets/images/programming.png',),
+                    backgroundImage: AssetImage(
+                      'assets/images/programming.png',
+                    ),
                     backgroundColor: Colors.transparent,
                   ),
                 ),
-                Text("𝚆𝚎𝚕𝚌𝚘𝚖𝚎 𝚋𝚊𝚌𝚔!",style: TextStyle(color: Colors.white,fontSize: 17,fontWeight: FontWeight.bold),),
-                SizedBox(height: 5,),
-                Text("Ｐｌｅａｓｅ  ｌｏｇｉｎ  ｔｏ  ｙｏｕｒ  ａｃｃｏｕｎｔ",style: TextStyle(color: Colors.white,fontSize: 12),),
-                SizedBox(height: 30,),
-
-
+                const Text(
+                  "𝚆𝚎𝚕𝚌𝚘𝚖𝚎 𝚋𝚊𝚌𝚔!",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                const Text(
+                  "Ｐｌｅａｓｅ  ｌｏｇｉｎ  ｔｏ  ｙｏｕｒ  ａｃｃｏｕｎｔ",
+                  style: TextStyle(color: Colors.white, fontSize: 12),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
                 Form(
                   key: _formkey,
-                  child: Column(children: [
-                    Padding(
-                      padding: EdgeInsets.all(15),
-                      child: TextFormField(
-                        style: TextStyle(color: Colors.white),
-                        keyboardType: TextInputType.emailAddress,
-                        controller: userController,
-                        decoration: InputDecoration(
-                          labelStyle: TextStyle(color: Colors.white),
-                          hintStyle: TextStyle(color: Colors.grey),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color:Color.fromRGBO(128, 128, 128, 0.4),),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: TextFormField(
+                          style: const TextStyle(color: Colors.white),
+                          keyboardType: TextInputType.emailAddress,
+                          controller: userController,
+                          decoration: const InputDecoration(
+                            labelStyle: TextStyle(color: Colors.white),
+                            hintStyle: TextStyle(color: Colors.grey),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color.fromRGBO(128, 128, 128, 0.4),
+                              ),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color.fromRGBO(128, 128, 128, 0.4),
+                              ),
+                            ),
+                            labelText: 'User Name',
+                            hintText: 'Enter Your Name',
                           ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Color.fromRGBO(128, 128, 128, 0.4),),
-
-                          ),
-                          labelText: 'User Name',
-                          hintText: 'Enter Your Name',
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Enter Name';
+                            }
+                            return null;
+                          },
                         ),
-               validator: (value){
-                       if(value!.isEmpty){
-                         return 'Enter Name';
-                       }
-                       return null;
-                   },
                       ),
-                    ),
-
-                    Padding(
-                      padding: EdgeInsets.all(15),
-                      child: TextFormField(
-                        style: TextStyle(color: Colors.white),
-                        keyboardType: TextInputType.visiblePassword,
-                        controller: passwordController,
-                        decoration: InputDecoration(
-                          labelStyle: TextStyle(color: Colors.white),
-                          hintStyle: TextStyle(color: Colors.grey),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color:Color.fromRGBO(128, 128, 128, 0.4),),
+                      Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: TextFormField(
+                          style: const TextStyle(color: Colors.white),
+                          keyboardType: TextInputType.visiblePassword,
+                          controller: passwordController,
+                          decoration: const InputDecoration(
+                            labelStyle: TextStyle(color: Colors.white),
+                            hintStyle: TextStyle(color: Colors.grey),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color.fromRGBO(128, 128, 128, 0.4),
+                              ),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color.fromRGBO(128, 128, 128, 0.4),
+                              ),
+                            ),
+                            labelText: 'Paaspword',
+                            hintText: 'Enter Your Password',
                           ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Color.fromRGBO(128, 128, 128, 0.4),),
-
-                          ),
-                          labelText: 'Paaspword',
-                          hintText: 'Enter Your Password',
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Enter Password';
+                            }
+                            return null;
+                          },
                         ),
-                        validator: (value){
-                          if(value!.isEmpty){
-                            return 'Enter Password';
-                          }
-                          return null;
-                        },
                       ),
-                    ),
-                    Align(
-                        alignment: Alignment.centerRight,
-                        child: Text('forget password?',style: TextStyle(fontSize: 12,color: Colors.white),)),
-                  ],),
+                      const Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            'forget password?',
+                            style: TextStyle(fontSize: 12, color: Colors.white),
+                          )),
+                    ],
+                  ),
                 ),
-
-SizedBox(height: 40,),
+                const SizedBox(
+                  height: 40,
+                ),
                 GestureDetector(
-                  onTap: (){
-                    if(_formkey.currentState!.validate()){
-                   login();
-                    }
+                  onTap: () {
+                    ////--------- firebase login------------
+                    //   if(_formkey.currentState!.validate()){
+                    //  login();
+                    //   }
+
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => HomePage()));
                   },
                   child: Container(
                     alignment: Alignment.center,
                     padding: EdgeInsets.symmetric(vertical: 15),
-                    width:  MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(color: Color.fromRGBO(128, 128, 128, 0.4),),
-
-                    child:loading?CircularProgressIndicator(): Text('Login',style: TextStyle(color: Colors.white,fontSize: 17),),
-
-
+                    width: MediaQuery.of(context).size.width,
+                    decoration: const BoxDecoration(
+                      color: Color.fromRGBO(128, 128, 128, 0.4),
+                    ),
+                    child: loading
+                        ? const CircularProgressIndicator()
+                        : const Text(
+                            'Login',
+                            style: TextStyle(color: Colors.white, fontSize: 17),
+                          ),
                   ),
                 ),
-
-                SizedBox(height: 50,),
+                const SizedBox(
+                  height: 50,
+                ),
                 GestureDetector(
-                  onTap: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>RegisterNow()));
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => RegisterNow()));
                   },
-                  child: Row(
+                  child: const Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text("Don't have an account ?",style: TextStyle(color: Colors.white,fontSize: 17),),
-                      Text(" Register Now",style: TextStyle(color: Colors.grey,fontSize: 17),),
+                      Text(
+                        "Don't have an account ?",
+                        style: TextStyle(color: Colors.white, fontSize: 17),
+                      ),
+                      Text(
+                        " Register Now",
+                        style: TextStyle(color: Colors.grey, fontSize: 17),
+                      ),
                     ],
                   ),
                 ),
-
               ],
             ),
-
-
-
           ),
         ),
       ),
